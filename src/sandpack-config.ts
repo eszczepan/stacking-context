b9fe8003928026dd19e591f4f7d7bea54daf0308
Szczepan Szablej <szczepan.szablej@schibsted.com>
2023-02-23 23:27:59 +0100

    feat: use color variables

---
+++ src/sandpack-config.ts
@@ -0,0 +1,15 @@
+export const sandpackNarrowOptions = {
+  showLineNumbers: false, // default - true
+  showInlineErrors: true, // default - false
+  wrapContent: true, // default - false
+  editorHeight: 500, // default - 300
+  editorWidthPercentage: 65, // default - 50
+};
+
+export const sandpackWideOptions = {
+  showLineNumbers: false, // default - true
+  showInlineErrors: true, // default - false
+  wrapContent: true, // default - false
+  editorHeight: 500, // default - 300
+  editorWidthPercentage: 40, // default - 50
+};
